32b92b1070b0b34ad8ada2039cb0e51c655bc3e6
Arthur <arthurdearaujofaria@gmail.com>
2026-03-29 15:01:57 -0300

    Adiciona exercícios dos dias 1 e 2

---
+++ dia2.py
@@ -0,0 +1,14 @@
+nome = "Arthur"
+jogo_favorito = "Fifa"
+idade = 21
+
+print(f"Olá! Meu nome é {nome}.")
+print(f"Meu jogo favorito é {jogo_favorito}.")
+
+if idade >= 18:
+    print("Sou maior de idade.")
+else:
+    print("Sou menor de idade.")
+
+for i in range(3):
+    print(f"Linha número {i}")
